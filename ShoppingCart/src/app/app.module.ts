e32b0f62ea05580eac91f1305fbf336bdbd4b137
Joydev Das <iamjoydevdas@gmail.com>
2023-03-14 09:03:18 +0000

    add all

---
+++ ShoppingCart/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './core/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductComponent } from './shopping/product/product.component'
+import { HttpClientModule  } from "@angular/common/http";
+import { ProductService } from './shared/service/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule 
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
